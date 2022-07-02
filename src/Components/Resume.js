58751d3a655f7904d77fcacf817859a323d3c8e4
Ainagul Borambayeva <ainagulkzt@gmail.com>
2022-07-02 08:23:20 -0400

    services add

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,107 +1,57 @@
-import React, { Component } from "react";
-import Slide from "react-reveal";
+import React, {Component} from "react";
+import {FcBinoculars, FcPaid, FcApprove, FcInTransit, FcLikePlaceholder } from "react-icons/fc";
 
 class Resume extends Component {
-  getRandomColor() {
-    let letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
   render() {
     if (!this.props.data) return null;
 
-    const skillmessage = this.props.data.skillmessage;
-    const education = this.props.data.education.map(function (education) {
-      return (
-        <div key={education.school}>
-          <h3>{education.school}</h3>
-          <p className="info">
-            {education.degree} <span>&bull;</span>
-            <em className="date">{education.graduated}</em>
-          </p>
-          <p>{education.description}</p>
-        </div>
-      );
-    });
-
-    const work = this.props.data.work.map(function (work) {
-      return (
-        <div key={work.company}>
-          <h3>{work.company}</h3>
-          <p className="info">
-            {work.title}
-            <span>&bull;</span> <em className="date">{work.years}</em>
-          </p>
-          <p>{work.description}</p>
-        </div>
-      );
-    });
-
-    const skills = this.props.data.skills.map((skills) => {
-      const backgroundColor = this.getRandomColor();
-      const className = "bar-expand " + skills.name.toLowerCase();
-      const width = skills.level;
-
-      return (
-        <li key={skills.name}>
-          <span style={{ width, backgroundColor }} className={className}></span>
-          <em>{skills.name}</em>
-        </li>
-      );
-    });
-
     return (
-      <section id="resume">
-        <Slide left duration={1300}>
-          <div className="row education">
-            <div className="three columns header-col">
-              <h1>
-                <span>Education</span>
-              </h1>
-            </div>
+        <section id="resume" className="resume">
+          <div className="container">
+          <h2>Как сделать заказ</h2>
+            <div className="row">
+              <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+                <div className="icon-box">
+                <div className="icon"><FcBinoculars size ={70}/></div>
+                  <h4>Выбрать товар</h4>
+                  <p>Вам необходимо выбрать товар с онлайн-магазина по всем вашим пожеланиям и требованиям </p>
+                </div>
+              </div>
 
-            <div className="nine columns main-col">
-              <div className="row item">
-                <div className="twelve columns">{education}</div>
+              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+                <div className="icon-box">
+                  <div className="icon"><FcPaid size={70}/></div>
+                  <h4>Скинуть нам ссылку данного товара</h4>
+                  <p>Вам необходимо скинуть ссылку вашей корзины, куда вы разместили свои интересующие товары и скинуть чек об оплате данного заказа</p>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+                <div className="icon-box">
+                <div className="icon"><FcApprove size={70}/></div>
+                  <h4>Подтверждение заказа</h4>
+                  <p>В течение 24 часа мы обработаем ваш заказ и дадим вам подтверждение</p>
+                </div>
+              </div>
+
+              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                <div className="icon-box">
+                <div className="icon"><FcInTransit size ={70}/></div>
+                  <h4>Отправка товара</h4>
+                  <p>Как мы получим ваш товар мы сразу оформляем все необходимые документы и отправляем вам в вашу страну. После отправки товара вам необходимо скинуть чек об оплате за доставку. Мы работаем с надежной проверенной логистической компанией, которая работает с 1955 года, отмечается быстрой доставкой и не завышенными ценами
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                <div className="icon-box">
+                <div className="icon"><FcLikePlaceholder size={70}/></div>
+                  <h4>Наслаждайтесь!</h4>
+                  <p>Срок от обработки заказа до получения заказа в ваши руки занимает от 3 до 5 недель. Мы стараемся сделать весь процесс в короткие сроки, так как нацеленны на долгосрочные отношения и мы всегда рады довольным клиентам!</p>
+                </div>
+              </div>
               </div>
             </div>
-          </div>
-        </Slide>
-
-        <Slide left duration={1300}>
-          <div className="row work">
-            <div className="three columns header-col">
-              <h1>
-                <span>Work</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">{work}</div>
-          </div>
-        </Slide>
-
-        <Slide left duration={1300}>
-          <div className="row skill">
-            <div className="three columns header-col">
-              <h1>
-                <span>Skills</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <p>{skillmessage}</p>
-
-              <div className="bars">
-                <ul className="skills">{skills}</ul>
-              </div>
-            </div>
-          </div>
-        </Slide>
-      </section>
+        </section>
     );
   }
 }
