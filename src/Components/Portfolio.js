63eff9cf2cf58caac3bc37dfeee656df8c4cb077
Ainagul Borambayeva <ainagulkzt@gmail.com>
2022-07-02 11:59:49 -0400

    portfolio page fixes

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
 
@@ -7,14 +7,14 @@ class Portfolio extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const projects = this.props.data.projects.map(function (projects) {
+    const projects = this.props.data.projects.map(function(projects) {
       let projectImage = "images/portfolio/" + projects.image;
 
       return (
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
             <Zmage alt={projects.title} src={projectImage} />
-            <div style={{ textAlign: "center" }}>{projects.title}</div>
+            <div style={{textAlign: "center"}}>{projects.title}</div>
           </div>
         </div>
       );
@@ -25,7 +25,7 @@ class Portfolio extends Component {
         <Fade left duration={1000} distance="40px">
           <div className="row">
             <div className="twelve columns collapsed">
-              <h1>Check Out Some of My Works.</h1>
+              <h1>Онлайн-магазины</h1>
 
               <div
                 id="portfolio-wrapper"
